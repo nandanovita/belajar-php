9b3febb66e31c911c6e54f168c470dcadf0dd670
Nanda Novitasari <novitasariinanda@gmail.com>
2022-10-24 11:05:14 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Nanda";
+
+$umur = 20;
+
+echo  "Nama saya <strong>$nama</strong>, saya berusia $umur tahun. <br>";
+
+$namaAdik = "Niar";
+$umurAdik = "5";
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun. <br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih umur saya dan umur adik saya adalah $selisihUmur tahun.";
+
+?>
